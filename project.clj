9d29fb810e5fb70122f91c063d9cfc7373f0d90d
Yuri Klind <vykmnsk@gmail.com>
2014-07-18 21:11:42 +1000

    deps for zipper

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject xmldiffer "0.1.0-SNAPSHOT"
-  :description "Find differences between two XML files using XMLUnit"
+  :description "Test parsing xml"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.zip "0.1.1"]
+                 [org.clojure/data.xml "0.0.7"]
+                 [com.uswitch/clj-soap "0.2.3"]
                  [xmlunit/xmlunit "1.5"]]
-  :aot  [xmldiffer.core]
-  :main xmldiffer.core
-  )
+  :main xmldiffer.core)
